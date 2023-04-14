fff7060939c74c3c0951d76e05400a7dca9992d2
Henry Asa <henryjacobasa@gmail.com>
2023-04-14 18:39:05 -0400

    removing bad test file

--- tests/test_rules.py
+++
@@ -1,9 +0,0 @@
-
-new_rule = rules.Copy_To("mango", ["test1", "test2"])
-new_rule.add_attribute("hasTheWord", "Bla bla")
-# new_rule.add_attribute("shouldNeverSpam", "false")
-new_rule.add_attribute("doesNotHaveTheWord", "Three")
-new_rule.add_attribute("doesNotHaveTheWordMANGO", "Three", True)
-new_rule.add_attribute("subject", "Meow")
-print(new_rule.final_rule_str)
-# new_rule.
